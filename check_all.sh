9607a9813f11defc5810e320ff73bd00b1d6a89d
Bruno Stuyts <bruno@pro-found.be>
2026-08-24 20:27:23 +0200

    Added json-schemas for execution and processing metadata

--- check_all.sh
+++ check_all.sh
@@ -1,23 +1,29 @@
 cd examples
 
-check-jsonschema --schemafile ../schemas/psd_sieving_rawdata.json-schema psd_sieving_rawdata.json
+check-jsonschema --no-cache --schemafile ../schemas/psd_sieving_rawdata.json-schema psd_sieving_rawdata.json
 
-check-jsonschema --schemafile ../schemas/psd_sieving_conditions.json-schema psd_sieving_conditions.json
+check-jsonschema --no-cache --schemafile ../schemas/psd_sieving_conditions.json-schema psd_sieving_conditions.json
 
-check-jsonschema --schemafile ../schemas/psd_hydrometer_rawdata.json-schema psd_hydrometer_rawdata.json
+check-jsonschema --no-cache --schemafile ../schemas/psd_hydrometer_rawdata.json-schema psd_hydrometer_rawdata.json
 
-check-jsonschema --schemafile ../schemas/psd_hydrometer_conditions.json-schema psd_hydrometer_conditions.json
+check-jsonschema --no-cache --schemafile ../schemas/psd_hydrometer_conditions.json-schema psd_hydrometer_conditions.json
 
-check-jsonschema --schemafile ../schemas/psd_processed.json-schema psd_processed.json
+check-jsonschema --no-cache --schemafile ../schemas/psd_processed.json-schema psd_processed.json
 
-check-jsonschema --schemafile ../schemas/att_casagrande_rawdata.json-schema att_casagrande_rawdata.json
+check-jsonschema --no-cache --schemafile ../schemas/att_casagrande_rawdata.json-schema att_casagrande_rawdata.json
 
-check-jsonschema --schemafile ../schemas/att_casagrande_processed.json-schema att_casagrande_processed.json
+check-jsonschema --no-cache --schemafile ../schemas/att_casagrande_processed.json-schema att_casagrande_processed.json
 
-check-jsonschema --schemafile ../schemas/density_rawdata.json-schema density_rawdata.json
+check-jsonschema --no-cache --schemafile ../schemas/density_rawdata.json-schema density_rawdata.json
 
-check-jsonschema --schemafile ../schemas/density_processed.json-schema density_processed.json
+check-jsonschema --no-cache --schemafile ../schemas/density_processed.json-schema density_processed.json
 
-check-jsonschema --schemafile ../schemas/watercontent_rawdata.json-schema watercontent_rawdata.json
+check-jsonschema --no-cache --schemafile ../schemas/watercontent_rawdata.json-schema watercontent_rawdata.json
 
-check-jsonschema --schemafile ../schemas/watercontent_processed.json-schema watercontent_processed.json
+check-jsonschema --no-cache --schemafile ../schemas/watercontent_processed.json-schema watercontent_processed.json
+
+check-jsonschema --no-cache --schemafile ../schemas/organiccontent_rawdata.json-schema organiccontent_rawdata.json
+
+check-jsonschema --no-cache --schemafile ../schemas/organiccontent_processed.json-schema organiccontent_processed.json
+
+check-jsonschema --no-cache --schemafile ../schemas/carbonatecontent_hcl_rawdata.json-schema carbonatecontent_hcl_rawdata.json
