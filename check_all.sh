f81c9f7071cb828d905655e28451a4cb841fecab
Bruno Stuyts <bruno@pro-found.be>
2026-08-24 13:12:32 +0200

    Water content processed added

--- check_all.sh
+++ check_all.sh
@@ -11,3 +11,13 @@ check-jsonschema --schemafile ../schemas/psd_hydrometer_conditions.json-schema p
 check-jsonschema --schemafile ../schemas/psd_processed.json-schema psd_processed.json
 
 check-jsonschema --schemafile ../schemas/att_casagrande_rawdata.json-schema att_casagrande_rawdata.json
+
+check-jsonschema --schemafile ../schemas/att_casagrande_processed.json-schema att_casagrande_processed.json
+
+check-jsonschema --schemafile ../schemas/density_rawdata.json-schema density_rawdata.json
+
+check-jsonschema --schemafile ../schemas/density_processed.json-schema density_processed.json
+
+check-jsonschema --schemafile ../schemas/watercontent_rawdata.json-schema watercontent_rawdata.json
+
+check-jsonschema --schemafile ../schemas/watercontent_processed.json-schema watercontent_processed.json
