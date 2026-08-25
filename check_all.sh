2946d7690e10d49c40e27af7fe069222cdb5d139
Bruno Stuyts <bruno@pro-found.be>
2026-08-25 09:12:22 +0200

    Started adding test condition schemas

--- check_all.sh
+++ check_all.sh
@@ -1,35 +1,37 @@
 cd examples
 
+check-jsonschema --no-cache --schemafile ../schemas/att_casagrande_conditions.json-schema att_casagrande_conditions.json
+
 check-jsonschema --no-cache --schemafile ../schemas/att_casagrande_processed.json-schema att_casagrande_processed.json
 
 check-jsonschema --no-cache --schemafile ../schemas/att_casagrande_rawdata.json-schema att_casagrande_rawdata.json
 
-check-jsonschema --no-cache --schemafile ../schemas/carbonatecontent_hcl_processed.json-schema carbonatecontent_hcl_processed.json
+# check-jsonschema --no-cache --schemafile ../schemas/carbonatecontent_hcl_processed.json-schema carbonatecontent_hcl_processed.json
 
 check-jsonschema --no-cache --schemafile ../schemas/carbonatecontent_hcl_rawdata.json-schema carbonatecontent_hcl_rawdata.json
 
-
-check-jsonschema --no-cache --schemafile ../schemas/psd_sieving_rawdata.json-schema psd_sieving_rawdata.json
-
-check-jsonschema --no-cache --schemafile ../schemas/psd_sieving_conditions.json-schema psd_sieving_conditions.json
-
-check-jsonschema --no-cache --schemafile ../schemas/psd_hydrometer_rawdata.json-schema psd_hydrometer_rawdata.json
-
-check-jsonschema --no-cache --schemafile ../schemas/psd_hydrometer_conditions.json-schema psd_hydrometer_conditions.json
-
-check-jsonschema --no-cache --schemafile ../schemas/psd_processed.json-schema psd_processed.json
-
-
+check-jsonschema --no-cache --schemafile ../schemas/density_processed.json-schema density_processed.json
 
 check-jsonschema --no-cache --schemafile ../schemas/density_rawdata.json-schema density_rawdata.json
 
-check-jsonschema --no-cache --schemafile ../schemas/density_processed.json-schema density_processed.json
-
-check-jsonschema --no-cache --schemafile ../schemas/watercontent_rawdata.json-schema watercontent_rawdata.json
-
-check-jsonschema --no-cache --schemafile ../schemas/watercontent_processed.json-schema watercontent_processed.json
+check-jsonschema --no-cache --schemafile ../schemas/organiccontent_processed.json-schema organiccontent_processed.json
 
 check-jsonschema --no-cache --schemafile ../schemas/organiccontent_rawdata.json-schema organiccontent_rawdata.json
 
-check-jsonschema --no-cache --schemafile ../schemas/organiccontent_processed.json-schema organiccontent_processed.json
+check-jsonschema --no-cache --schemafile ../schemas/psd_hydrometer_conditions.json-schema psd_hydrometer_conditions.json
+
+check-jsonschema --no-cache --schemafile ../schemas/psd_hydrometer_rawdata.json-schema psd_hydrometer_rawdata.json
+
+check-jsonschema --no-cache --schemafile ../schemas/psd_processed.json-schema psd_processed.json
+
+check-jsonschema --no-cache --schemafile ../schemas/psd_sieving_conditions.json-schema psd_sieving_conditions.json
+
+check-jsonschema --no-cache --schemafile ../schemas/psd_sieving_rawdata.json-schema psd_sieving_rawdata.json
+
+check-jsonschema --no-cache --schemafile ../schemas/watercontent_processed.json-schema watercontent_processed.json
+
+check-jsonschema --no-cache --schemafile ../schemas/watercontent_rawdata.json-schema watercontent_rawdata.json
+
+
+
 
